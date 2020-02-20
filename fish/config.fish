7715dc84e214cbf388eca940d4408fc41424536d
davido-bs <david.oliver@strategic-i.com>
2020-02-19 19:24:09 -0800

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,11 @@
-set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+# vi key bindings
+fish_vi_key_bindings
+
+# let theme handle virtualenvs
+set -xg VIRTUAL_ENV_DISABLE_PROMPT 1
+
+# colors
 set theme_color_scheme base16-dark
+
+# editor
+set -xg EDITOR vim
