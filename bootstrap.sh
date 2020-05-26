318a4129b82e0fdf2127648068f8e5b082a9bf59
davidjoliver86 <davidjoliver86@gmail.com>
2020-05-26 08:57:35 -0700

    if i must switch to gnome

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update
-apt install -y python3-apt python3-venv git aptitude
+apt install -y python3-apt python3-venv python3-psutil git aptitude
 /usr/bin/python3 -m venv /usr/lib/virtualenvs/ansible --system-site-packages
 
 /usr/lib/virtualenvs/ansible/bin/pip install ansible
